f4d984cb6569f3faf5c4c86efa444777356850f4
Lionel <liolisena@gmail.com>
2024-09-23 12:11:44 +0100

    v0.0.2

--- llama_app/views.py
+++ llama_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
-# Create your views here.
+# Load model directly
+tokenizer = AutoTokenizer.from_pretrained("arcee-ai/Llama-3.1-SuperNova-Lite")
+model = AutoModelForCausalLM.from_pretrained("arcee-ai/Llama-3.1-SuperNova-Lite")
+
+def generate_text(request):
+    if request.method == 'POST':
+        input_text = request.POST.get('input_text', '')
+        
+        # Tokenize l'entrée
+        inputs = tokenizer(input_text, return_tensors="pt")
+        
+        # Générez la sortie
+        with torch.no_grad():
+            outputs = model.generate(**inputs, max_length=100)
+        
+        # Décodez la sortie
+        generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        
+        return render(request, 'llama_app/index.html', {'input_text': input_text, 'generated_text': generated_text})
+    
+    return render(request, 'llama_app/index.html')
--- llama_project/settings.py
+++ llama_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'llama_app',
 ]
 
 MIDDLEWARE = [
@@ -68,7 +69,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'llama_project.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
